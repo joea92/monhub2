4b26fb8cbbce3222a9386990cf1e3d9bc5d7d150
Joe Agius <agius.joe@gmail.com>
2026-03-18 01:19:22 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { Search, Wrench, MapPin, GitCompare, ArrowRight, Star } from 'lucide-react';
 
