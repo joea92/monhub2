6f20de649b6a62716b2eca848b8f909328635148
Joe Agius <agius.joe@gmail.com>
2026-03-18 08:10:09 +0000

    File changes

--- functions/updatePokemonImagesFromDropbox.ts
+++ functions/updatePokemonImagesFromDropbox.ts
@@ -48,7 +48,7 @@ Deno.serve(async (req) => {
         const record = allRecords.find(r => r.name.toLowerCase() === filename.toLowerCase());
 
         if (!record) {
-          failed++;
+          notInApp++;
           continue;
         }
 
