73da0658ebd7cd93f14c41738e343428ae9f31cf
Joe Agius <agius.joe@gmail.com>
2026-03-18 14:22:39 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -145,7 +145,7 @@ export default function Layout() {
         </button>
         </nav>
 
-
+      <DonationModal open={showDonationModal} onClose={() => setShowDonationModal(false)} />
     </div>
   );
 }
