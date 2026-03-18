e158a8a9e74de7edcf1a569d4a34f55f283c2242
Joe Agius <agius.joe@gmail.com>
2026-03-18 10:33:55 +0000

    File changes

--- src/lib/pokemonData.js
+++ src/lib/pokemonData.js
@@ -77,6 +77,7 @@ const SILHOUETTE_IMAGES = {
   "dragonite": "https://media.base44.com/images/public/69b976fdec8fc338dd963cb9/1b29cb978_Dragonite.png",
   "dratini": "https://media.base44.com/images/public/69b976fdec8fc338dd963cb9/0471a0c89_Dratini.png",
   "drifloon": "https://media.base44.com/images/public/69b976fdec8fc338dd963cb9/45dd07eed_Drifloon.png",
+  "drifblim": "https://media.base44.com/images/public/69b976fdec8fc338dd963cb9/865949af6_Drifblim.png",
   "drowzee": "https://media.base44.com/images/public/69b976fdec8fc338dd963cb9/934b3fc1e_Drowzee.png",
   "duskull": "https://media.base44.com/images/public/69b976fdec8fc338dd963cb9/5aa2e15c5_Duskull.png",
   "dusclops": "https://media.base44.com/images/public/69b976fdec8fc338dd963cb9/b25ee35f0_Dusclops.png",
